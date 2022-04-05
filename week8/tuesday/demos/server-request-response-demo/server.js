2f21241cb59d078aacd54607ff4c48710c7d9795
Javier Ortiz <javier.ortiz989@gmail.com>
2022-04-05 12:32:36 -0700

    static asset

--- week8/tuesday/demos/server-request-response-demo/server.js
+++ week8/tuesday/demos/server-request-response-demo/server.js
@@ -5,9 +5,15 @@ const server = http.createServer((req, res) => {
     if (req.method === 'GET' && req.url === '/') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html');
-        return res.end('<h1>Hello world!</h1>');
+        return res.end('Dog Club');
     }
     
+    if (req.method === 'GET' && req.url === '/posts') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        return res.end('<h1>Posts page</h1>');
+    }
+
     res.statusCode = 404;
     res.setHeader('Content-Type', 'text/html')
     res.end('Page not found =(');
