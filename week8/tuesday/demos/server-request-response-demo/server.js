f4faca086cbe72077a53b97a47807e49bac45de1
Javier Ortiz <javier.ortiz989@gmail.com>
2022-04-05 10:02:13 -0700

    first server

---
+++ week8/tuesday/demos/server-request-response-demo/server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log(`${req.method} ${req.url}`);
+    if (req.method === 'GET' && req.url === '/') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        return res.end('<h1>Hello world!</h1>');
+    }
+    
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/html')
+    res.end('Page not found =(');
+});
+
+const port = 5002;
+server.listen(port, () => console.log('Server is listening in port', port));
