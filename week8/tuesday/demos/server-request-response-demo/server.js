65521db868cb47c751b8f58256451f2bb3ef0531
Javier Ortiz <javier.ortiz989@gmail.com>
2022-04-05 13:33:20 -0700

    post requests

--- week8/tuesday/demos/server-request-response-demo/server.js
+++ week8/tuesday/demos/server-request-response-demo/server.js
@@ -11,9 +11,50 @@ const server = http.createServer((req, res) => {
         return res.end(htmlPage);
     }
 
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/html')
-    res.end('Page not found =(');
+    if (req.method === "GET" && req.url === "/main.css") {
+        const resBody = fs.readFileSync("main.css");
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/css");
+        return res.end(resBody);
+    }
+
+    let reqBody = "";
+    req.on("data", (data) => {
+        reqBody += data;
+    });
+
+    req.on("end", () => {
+        // Parsing the body of the request
+        console.log(reqBody);
+        if (reqBody) {
+            req.body = reqBody.split("&")
+                .map((keyValuePair) => keyValuePair.split("="))
+                .map(([key, value]) => [key, value.replace(/\+/g, " ")])
+                .map(([key, value]) => [key, decodeURIComponent(value)])
+                .reduce((acc, [key, value]) => {
+                    acc[key] = value;
+                    return acc;
+                }, {});
+            console.log(req.body);
+            // app.use(express.urlencoded())
+        }
+
+        if (req.method === "POST" && req.url === "/tasks") {
+            console.log(req.body);
+            // database.push(req.body);
+            // res.statusCode = 302;
+            // res.setHeader("Location", "/tasks");
+            return res.end();
+        }
+
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html')
+        res.end('Page not found =(');
+    });
+
+    
+    
+    
 });
 
 const port = 5002;
