a73cd9ee504307781f423d132d3274f18036ab52
Javier Ortiz <javier.ortiz989@gmail.com>
2022-04-12 10:13:37 -0700

    element selection

--- week9/tuesday/demo/element-selection-and-manipulation-demo/index.js
+++ week9/tuesday/demo/element-selection-and-manipulation-demo/index.js
@@ -3,22 +3,26 @@ window.onload = () => {
         const ball = document.getElementById("ball");
         console.log(ball);
         ball.classList.add("red-ball");
-        
-        // const instructor = document.querySelector(".instructor");
-        // console.log(instructor);
-        // instructor.style.color = "red";
+        // ball.setAttribute('class', 'red-ball');
+
+        const instructor = document.querySelector(".instructor");
+        console.log(instructor);
+        instructor.style.color = "red";
+        // vs
+        // instructor.style = "color: red;"
 
         const instructors = document.querySelectorAll(".instructor");
         console.log(instructors);
         instructors.forEach(instructor => {
             instructor.style.color = "red";
         });
-
+        
+        const url = 'https://p4.wallpaperbetter.com/wallpaper/70/625/78/animal-baby-cute-dog-wallpaper-preview.jpg';
         const playground = document.getElementById("playground");
         playground.innerHTML = `
             <h2 class='red'>Hope you enjoyed the demo!</h2>
             <img 
-                src='https://p4.wallpaperbetter.com/wallpaper/70/625/78/animal-baby-cute-dog-wallpaper-preview.jpg'
+                src=${url}
             >
         `;
 
