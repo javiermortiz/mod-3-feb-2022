a077b3d31e3aa5f5a28101f0e4947d16d66c32ea
Javier Ortiz <javier.ortiz989@gmail.com>
2022-04-13 17:00:41 -0700

    catsagram

--- week9/wednesday/solutions/data-in-elements/solution/data.js
+++ week9/wednesday/solutions/data-in-elements/solution/data.js
@@ -1,11 +1,14 @@
 window.addEventListener("DOMContentLoaded", event => {
+    console.log('domcontentloaded');
     const shoppingList = document.getElementById("shopping-list");
     document.getElementById("add")
         .addEventListener("click", e => {
+            console.log('clicking');
             e.preventDefault();
             const name = document.getElementById("name");
+            console.log(name);
             const type = document.getElementById("type");
-
+            console.log(name.value, type.value);
             const li = document.createElement("li");
             li.innerText = name.value;
             li.dataset.type = type.value;
